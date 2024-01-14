b27616b14d30c915b4c71a0bafc6c41210a037ce
hiokky <hiokky.work@gmail.com>
2024-01-15 01:13:09 +0900

    add tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'tailwindcss/tailwind.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+        <h1 className="text-3xl font-bold text-red-500 underline">
+      Hello world!
+    </h1>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
